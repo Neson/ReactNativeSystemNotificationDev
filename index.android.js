bbda8c2c0dc5f620b1d4a1ce54d96cf95d09c24c
Neson <neson@dex.tw>
2016-01-17 01:34:52 +0800

    write testing UI

--- index.android.js
+++ index.android.js
@@ -1,52 +1,105 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
-
-var React = require('react-native');
-var {
+import React, {
   AppRegistry,
+  DeviceEventEmitter,
   StyleSheet,
-  Text,
   View,
-} = React;
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableOpacity
+} from 'react-native';
+import Notification from 'react-native-system-notification';
+
+const defaultNotificationSendArgs = `{
+  "id": null,
+  "subject": "Subject",
+  "message": "Message",
+  "sound": "default",
+  "vibrate": "default",
+  "lights": "default"
+}`;
 
 var ReactNativeSystemNotificationDev017 = React.createClass({
+  getInitialState: function() {
+    return {
+      notificationSendArgs: defaultNotificationSendArgs
+    };
+  },
+
+  componentWillMount: function() {
+    DeviceEventEmitter.addListener('sysNotificationClick', function(e) {
+      console.log('sysNotificationClick', e);
+      React.ToastAndroid.show(`Notification Clicked: ${e.action}: ${JSON.stringify(e, null, 2)}`, React.ToastAndroid.LONG);
+    });
+  },
+
+  _handleCreateNotification: function() {
+    var notification = {};
+
+    try {
+      notification = JSON.parse(this.state.notificationSendArgs);
+    } catch (e) {
+      React.ToastAndroid.show(`Error: ${e}`, React.ToastAndroid.LONG);
+    }
+
+    Notification.create(notification).then(function (n) {
+      React.ToastAndroid.show(`Notification Created: ${JSON.stringify(n, null, 2)}`, React.ToastAndroid.LONG);
+    }).catch((e) => {
+      React.ToastAndroid.show(`Error: ${e}`, React.ToastAndroid.LONG);
+    });
+  },
+
   render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <ScrollView contentContainerStyle={styles.container}>
+        <View style={styles.inputBox}>
+          <TextInput
+            onChangeText={(notificationSendArgs) => this.setState({ notificationSendArgs })}
+            multiline={true}
+            autoCorrect={false}
+            autoCapitalize="none"
+            value={this.state.notificationSendArgs}
+            underlineColorAndroid="transparent"
+            numberOfLines={this.state.notificationSendArgs.split(/\r\n|\r|\n/).length}
+            textAlign="start"
+            textAlignVertical="top"
+            style={styles.commandInput}
+          />
+        </View>
+        <TouchableOpacity
+          style={styles.actionButton}
+          onPress={this._handleCreateNotification}
+        >
+          <Text style={styles.actionButtonText}>Create Notification</Text>
+        </TouchableOpacity>
+      </ScrollView>
     );
   }
 });
 
-var styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    padding: 16,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  inputBox: {
+    borderColor: '#AAAAAA',
+    borderWidth: 1,
+    borderRadius: 2,
+    backgroundColor: '#FFFFFF'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  commandInput: {
+    fontFamily: 'monospace',
+    fontSize: 14,
+    lineHeight: 18
   },
+  actionButton: {
+    alignSelf: 'flex-end',
+    paddingHorizontal: 16,
+    paddingVertical: 8
+  },
+  actionButtonText: {
+    fontSize: 18
+  }
 });
 
 AppRegistry.registerComponent('ReactNativeSystemNotificationDev017', () => ReactNativeSystemNotificationDev017);
